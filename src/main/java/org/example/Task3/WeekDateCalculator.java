0ff12b27ddc9cffb0735b5be43c9d33a9d1491e3
xHlpNw <aleksashin990@gmail.com>
2025-11-23 16:59:00 +0400

    makes WeekDateCalculator methods static

--- src/main/java/org/example/Task3/WeekDateCalculator.java
+++ src/main/java/org/example/Task3/WeekDateCalculator.java
@@ -2,16 +2,15 @@ package org.example.Task3;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.temporal.WeekFields;
 
 public class WeekDateCalculator {
-    public LocalDate getMonday(int year, int week) {
+    public static LocalDate getMonday(int year, int week) {
         LocalDate jan4 = LocalDate.of(year, 1, 4);
         LocalDate firstMonday = jan4.with(DayOfWeek.MONDAY);
         return firstMonday.plusWeeks(week - 1);
     }
 
-    public LocalDate getSunday(int year, int week) {
+    public static LocalDate getSunday(int year, int week) {
         return getMonday(year, week).plusDays(6);
     }
 }
